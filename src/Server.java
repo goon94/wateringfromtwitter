531c58404438b4309835407d1a48a233a819f7fc
Cristiano <cristianobarbosa1996@gmail.com>
2018-04-15 23:57:29 +0100

    project started 2

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,43 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.*;
+import java.io.*;
+
+
 public class Server {
 
-    public static void main(String args[]){
-        
+    static Connection mySQLconnection;
+
+    public static void main(String args[]) throws IOException {
+
+
+
+        String clientSentence;
+        String responseToClient;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            //System.out.println("Received: " + clientSentence);
+            responseToClient = "";
+
+
+
+
+
+            responseToClient = responseToClient + '\n';
+            outToClient.writeBytes(responseToClient);
+        }
+
+
+
 
     }
+
 
 }
